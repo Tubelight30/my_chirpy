842ad8dd2358d8163b9975dda5039c5a2c0605f9
Tubelight30 <soumaygupta2004@gmail.com>
2024-07-11 10:36:56 +0530

    serves a file called index.html

--- main.go
+++ main.go
@@ -6,12 +6,16 @@ import (
 )
 
 func main() {
+	const filepathRoot = "."
+	const port = "8080"
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    ":" + port,
 		Handler: mux,
 	}
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 
 }
